c4f4fd7d9398624215fcbea12e31cb206b443c10
kradova <ksenia.radova@gmail.com>
2022-06-22 00:01:22 +0300

    + asserting the app state

--- test-3.cy.js
+++ test-3.cy.js
@@ -1,8 +1,10 @@
 //Interacting with the Todo input element(pg 80)
-describe('empty spec', () => {
-    it('Adds a new ToDo', () => {
-        cy.visit('http://todomvc.com/examples/react/#/');
+describe("empty spec", () => {
+    it("Adds a new ToDo", () => {
+        cy.visit("http://todomvc.com/examples/react/#/");
 
-        cy.get('.new-todo').type('New ToDo {enter}');
+        cy.get(".new-todo").type("New Todo {enter}");
+        cy.get(".new-todo").type("Anoter Todo{enter}");
+        cy.get(".todo-list").find("li").should("have.length", 3);
     });
 });
