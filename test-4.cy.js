695ce78f1cb3686720baba2bed198fc129421032
kradova <ksenia.radova@gmail.com>
2022-06-24 23:40:16 +0300

    + method .should()

---
+++ test-4.cy.js
@@ -0,0 +1,9 @@
+describe("Assertions", () => {
+    it("Implicit subjects - should", () => {
+        cy.visit("http://todomvc.com/examples/react/#/");
+
+        cy.get(".new-todo")
+            .should("have.attr", "placeholder",
+                "What needs to be done?");
+    });
+});
