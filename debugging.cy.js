33b8b9390de66e750435947d4d53d971071b5d10
kradova <ksenia.radova@gmail.com>
2022-06-26 23:22:43 +0300

    + debugging error

--- debugging.cy.js
+++ debugging.cy.js
@@ -1,6 +1,13 @@
 it('can add a todo', () => {
     cy.visit("http://todomvc.com/examples/react/#/");
-    
+
     cy.get(".new-todo").type("New Todo {Enter}");
     cy.get(".todo-list").find('li').should('have.length', 1);
+});
+
+it('Error Test: can add a todo', () => {
+    cy.visit("http://todomvc.com/examples/react/#/");
+
+    cy.get(".new-todo").type("New Todo {Enter}");
+    cy.get(".todo-list").find('li').should('have.length', 2);
 });
