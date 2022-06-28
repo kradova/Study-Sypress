53c4715250a1e234a525be592680356b0682182c
kradova <ksenia.radova@gmail.com>
2022-06-28 23:52:07 +0300

    + debug() func

--- debugging.cy.js
+++ debugging.cy.js
@@ -1,4 +1,4 @@
-it('can add a todo', () => {
+it.skip('can add a todo', () => {
     cy.visit("http://todomvc.com/examples/react/#/");
 
     cy.get(".new-todo").type("New Todo {Enter}");
@@ -13,8 +13,20 @@ it.skip('Error Test: can add a todo', () => {
 });
 
 it('can mark a todo as completed', () => {
+    cy.visit("http://todomvc.com/examples/react/#/");
+
     cy.get(".new-todo").type("New Todo {Enter}");
     cy.get(".new-todo").type("Another New Todo {Enter}");
     cy.get('.todo-list>li:nth-child(1)').find('.toggle').click();
     cy.get('.todo-list>li:nth-child(2)').find('.toggle').click();
 });
+
+it('Special commands-debug : can mark a todo as completed',
+    () => {
+        cy.visit("http://todomvc.com/examples/react/#/");
+
+        cy.get(".new-todo").type("New Todo {Enter}");
+        cy.get(".new-todo").type("Another New Todo {Enter}").debug();
+        cy.get('.todo-list>li:nth-child(1)').find('.toggle').click();
+        cy.get('.todo-list>li:nth-child(2)').find('.toggle').click();
+    });
