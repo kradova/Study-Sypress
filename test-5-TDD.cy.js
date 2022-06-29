d5c94a03752fc452f5e2aa1954857546fd53ed9c
kradova <ksenia.radova@gmail.com>
2022-06-29 20:14:25 +0300

    + TDD delete new todo

--- test-5-TDD.cy.js
+++ test-5-TDD.cy.js
@@ -1,7 +1,18 @@
-it('can create and display new todo', () => {
+it.skip('can create and display new todo', () => {
     cy.get('[data-testid="todo-item-input"]')
         .type('New todo');
     cy.get('[data-testid="add-todo-button"]')
         .click();
     cy.contains('New Todo');
 });
+
+it('can delete added todo item', () => {
+    cy.get('[data-testid="todo-item-input"]')
+        .type('New todo');
+    cy.get('[data-testid="add-todo-button"]')
+        .click();
+    cy.get('[data-testid="delete-todo-1-button"]')
+        .click();
+    expect('[data-testid="todolist"]' ).not.to.contain('New Todo');
+});
+
