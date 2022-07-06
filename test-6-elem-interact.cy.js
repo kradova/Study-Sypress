8dc83e37dccd0828875715ccded104aa92a6daa2
kradova <ksenia.radova@gmail.com>
2022-07-07 00:00:08 +0300

    + no forced toggle option

--- test-6-elem-interact.cy.js
+++ test-6-elem-interact.cy.js
@@ -6,3 +6,11 @@ it('can mark a todo as completed - with changed hitbox position', () => {
         .find('.toggle').click({ position: 'topLeft' });
 });
 
+it('can mark all todo as completed - with no forced toggle option (Failure)', () => {
+    cy.get(".new-todo").type("New Todo {Enter}");
+    cy.get(".new-todo").type("Another New Todo {Enter}");
+    cy.get('.todo-list>li:nth-child(1)')
+        .find('.toggle').click();
+    cy.get('#toggle-all').click();
+});
+
