f051e964a07dc4770f0f8b655d58396877712222
kradova <ksenia.radova@gmail.com>
2022-08-04 19:46:51 +0300

    + cypres variable var

--- test-7.cy.js
+++ test-7.cy.js
@@ -9,7 +9,7 @@ it.skip('carries out asynchronous events', () => {
     button.click();
 });
 
-it('can Add todo item - (Closures)', () => {
+it.skip('can Add todo item - (Closures)', () => {
     cy.visit('http://todomvc.com/examples/react/#/')
     cy.get(".new-todo").type("New Todo {Enter}");
     cy.get('.todo-list>li:nth-child(1)').then(($todoItem) => {
@@ -22,3 +22,23 @@ it('can Add todo item - (Closures)', () => {
     cy.get(".new-todo").type("Another New Todo {Enter}");
 });
 
+describe('Cypress Variables', () => {
+    beforeEach(() => {
+        // Cypress starts out with a blank slate for each test
+        // so we must tell it to visit our website with the `cy.visit()` command.
+        // Since we want to visit the same URL at the start of all our tests,
+        // we include it in our beforeEach function so that it runs before each test
+        cy.visit('http://todomvc.com/examples/react/#/')
+    })
+    
+    var a = 20;
+    it('var scope context', () => {
+        a = 30; // overriding global scope
+        expect(a).to.eq(30) // a = 30
+    });
+    it('var scope context - changed context', () => {
+        // Variable scope remains the same as the change affects
+        // the global scope
+        expect(a).to.eq(30) //a = 30
+    });
+});
