0056c15606f99288373d09cc974a32e31e463d97
kradova <ksenia.radova@gmail.com>
2022-08-05 16:44:55 +0300

    + cypres aliases .invoke().as()

--- test-8.cy.js
+++ test-8.cy.js
@@ -2,7 +2,7 @@
  * Chapter 8. Aliases
  */
 
-context('TODO MVC - Aliases Tests', () => {
+context.skip('TODO MVC - Aliases Tests', () => {
     let text;
 
     beforeEach(() => {
@@ -12,9 +12,21 @@ context('TODO MVC - Aliases Tests', () => {
             text = $todoItem.text()
         });
     });
-    
+
     it('gets added todo item', () => {
         // todo item text is available for use
         expect(text).to.eq('New Todo')
+    });
+});
+
+describe('Sharing Context between hooks and tests', () => {
+    beforeEach(() => {
+        cy.visit('http://todomvc.com/examples/react/#/');
+        cy.get(".new-todo").type("New Todo {Enter}");
+        cy.get('.todo-list>li:nth-child(1)').invoke('text').as('todoItem');
+    });
+    it('gets added todo item', function () {
+        // todo item text is available for use
+        expect(this.todoItem).to.eq('New Todo');
     });
 });
